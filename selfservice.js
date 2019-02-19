7eb640587119a01fab8514ed9e0746c01ff522f3
jbergamini <jbergamini@jeff.cis.cabrillo.edu>
2019-02-18 18:40:35 -0800

    Added support for extracting add codes

--- selfservice.js
+++ selfservice.js
@@ -51,6 +51,26 @@ async function getSectionStudents(TERM, browser, sectionLink) {
   return {courseID: courseID, roster: studentInfo};
 }
 
+async function getSectionAddCodes(TERM, browser, sectionLink) {
+  const page = await browser.newPage();
+  await page.goto(sectionLink);
+  await page.waitForSelector('#faculty-roster-table > tbody:nth-child(2)');
+  const sectionTitle = await page.$eval('#user-profile-name', element => element.textContent);
+  const courseID =
+    sectionTitle.toLowerCase().split(/\s/)[0].split('-').slice(0, 2).join('') +
+    TERM.toLowerCase()[0] + TERM.substring(TERM.length - 2);
+  const sectionID = sectionTitle.split(/\s/)[0].split('-').pop().replace(/[^A-Za-z0-9]/g, '');
+  await page.waitForSelector('#permissionsTab > a');
+  await page.click('#permissionsTab > a');
+  await page.waitForSelector('#faculty-permissions-nav > div.clear-group > div > div:nth-child(4) > a');
+  await page.click('#faculty-permissions-nav > div.clear-group > div > div:nth-child(4) > a');
+  await page.waitForSelector('#authorization-29-code');
+  await page.screenshot({ path: 'example.png', fullPage: true });
+  const addCodes = await page.evaluate(() => Array.from(document.querySelectorAll('td')).filter(cell => cell.getAttribute('data-role') === 'Authorization Code').map(cell => cell.textContent.trim()));
+  await page.close();
+  return {courseID: courseID, sectionID: sectionID, addCodes: addCodes};
+}
+
 module.exports.getRosters = async function (USERNAME, PASSWORD, TERM) {
   [browser, page] = await login(USERNAME, PASSWORD, TERM);
   sectionLinks = await getSectionLinks(TERM, page);
@@ -61,4 +81,17 @@ module.exports.getRosters = async function (USERNAME, PASSWORD, TERM) {
   }
   await browser.close();
   return rosters;
+}
+
+module.exports.getAddCodes = async function (USERNAME, PASSWORD, TERM) {
+  [browser, page] = await login(USERNAME, PASSWORD, TERM);
+  sectionLinks = await getSectionLinks(TERM, page);
+  const allAddCodes = [];
+  for (let i = 0; i < sectionLinks.length; ++i) {
+    const addCodes = await getSectionAddCodes(TERM, browser, sectionLinks[i]);
+    allAddCodes.push(addCodes);
+  }
+  await browser.close();
+  return allAddCodes;
+
 }
